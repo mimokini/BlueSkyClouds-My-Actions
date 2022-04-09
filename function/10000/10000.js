956d5b9b5a2bce91d4502ede66985a768a04ca76
mimokini <49512475+mimokini@users.noreply.github.com>
2022-04-10 01:20:54 +0800

    Update 10000.js

--- function/10000/10000.js
+++ function/10000/10000.js
@@ -14,7 +14,8 @@ const SEND_KEY = process.env.SEND_KEY
 const UTC8 = new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000;
 
 async function downFile () {
-    const url = 'https://raw.githubusercontent.com/chavyleung/scripts/master/10000/10000.js'
+//    const url = 'https://raw.githubusercontent.com/chavyleung/scripts/master/10000/10000.js'
+    const url = 'https://github.com/chavyleung/scripts/blob/master/10000/10000.js'
     await download(url, './')
 }
 
